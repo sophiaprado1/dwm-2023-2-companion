8160dbea7c0b13695ad1f10e00319241912712b6
LuFi-1227 <luizsalvesp@protonmail.com>
2023-11-06 08:34:41 -0300

    Adicionada a tela de contatos e adicionada tela de login para cadastro de itens no banco de dados

---
+++ project/controller/controlPanel2.php
@@ -0,0 +1,35 @@
+<?php
+
+use control as GlobalControl;
+
+if(isset($_POST['login'])){
+    if($_POST['login']==1){
+        $user = $_POST['user'];
+        $password = $_POST['Pass'];
+        $ctrl = new control();
+        $ctrl->login($user, $password);
+    }
+}
+
+class control{
+    public function login($user, $password){
+        require "../model/Data.php";
+        $dataBank = new data();
+        $response = mysqli_fetch_assoc($dataBank->login($user, $password));
+        echo $response;
+        if($response != 0){
+            session_start();
+            $vetor = [
+                "userName" => $user,
+                "passWord" => $password
+            ];
+            $_SESSION['login'] = 1;
+            $_SESSION['data'] = json_encode($vetor);
+            header("Location: ../view/Adm/view/cadUsers.php");
+        }else{
+            header("Refresh: 0");
+            echo "Login ou senha incorretos";
+        }
+    }
+}
+?>
